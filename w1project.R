b4a543c43985e837d54624b2e435420c6603c27d
koalatif <valatif@gmail.com>
2016-12-14 21:37:33 +0000

    Update w1project.R

--- w1project.R
+++ w1project.R
@@ -1,38 +1,48 @@
 # Week1-project-of-course4
-Base_C4_W1 <- 
-  read.table("C:/Users/USER/Desktop/project_C4_W1/exdata_2Fdata%2Fhousehold_power_consumption/household_power_consumption.txt",
+data <- read.table("C:/Users/USER/Desktop/project_C4_W1/exdata_2Fdata%2Fhousehold_power_consumption/household_power_consumption.txt",
              header=TRUE, sep=";", na.strings="NA", dec=".", strip.white=TRUE)
-activepower=Base_C4_W1[, 3]
-as.numeric(Base_C4_W1$Global_active_power)
-as.numeric(activepower)
-with(Base_C4_W1, hist(as.numeric(activepower), scale="frequency", breaks="Sturges", 
-                      col="red"))
-date=Base_C4_W1[, 1]
+Base<- subset(data, data$Date=="1/2/2007"|data$Date=="2/2/2007")
+Base$Global_active_power <- as.numeric(as.character(Base$Global_active_power))
+Base$Global_reactive_power <- as.numeric(as.character(Base$Global_reactive_power))
+Base$Voltage <- as.numeric(as.character(Base$Voltage))
+Base$Sub_metering_1 <- as.numeric(as.character(Base$Sub_metering_1))
+Base$Sub_metering_2 <- as.numeric(as.character(Base$Sub_metering_2))
+Base$Sub_metering_3 <- as.numeric(as.character(Base$Sub_metering_3))
+Base$Datetime <-paste(Base$Date, Base$Time)
+
+#Plot1.png
+
+png("plot1.png", width=480, height=480)
+hist(Base$Global_active_power, col="red", main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
+
+#Plot2.png
+
+png("plot2.png", width=480, height=480)
+plot(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Global_active_power, type="l", xlab="",ylab="Global Active Power(Kilowatts)" )
+dev.off()
+
+#Plot3.png
 
 
 
+png("plot3.png", width=480, height=480)
+plot(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Sub_metering_2, type = "l", col="red")
+lines(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Sub_metering_3, type = "l", col="blue")
+legend("topright",lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
 
+#Plot4.png
 
-date=Base_C4_W1$Date
-time=Base_C4_W1$Time
-mydate=dmy(date)
-Base_C4_W1$mydate <- with(Base_C4_W1, dmy(date))
-class(mydate)
-mydays=weekdays(mydate)
-mydays
-
-Base_C4_W1$mydays <- with(Base_C4_W1, weekdays(mydate))
-mymonths=months(mydate)
-mymonths
-myyears=year(mydate)
-myyears
-
-set.seed(123)
-data <- data.frame(Date = Base_C4_W1$mydate ,
-                   Time = format(Base_C4_W1$Time ,
-                   Global_active_power = aggregate(data$Global_active_power, by(mydays), FUN=sum)))
-
-data$datetime <- as.POSIXct(paste(data$Date,data$Time),tz="UCT")
-plot.ts(data$datetime,Base_C4_W1$Global_active_power,type="l")
- 
+png("plot4.png", width=480, height=480)
+par(mfcol = c(2,2))
+plot(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Global_active_power, type="l", xlab="",ylab="Global Active Power(Kilowatts)" )
+plot(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Sub_metering_2, type = "l", col="red")
+lines(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Sub_metering_3, type = "l", col="blue")
+legend("topright",lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+plot(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+plot(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+dev.off()
  
