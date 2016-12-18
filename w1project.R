9316e2f9be2707f6046f0a3a5f6c8614ac99e761
koalatif <valatif@gmail.com>
2016-12-18 23:30:24 +0000

    Update w1project.R

--- w1project.R
+++ w1project.R
@@ -10,28 +10,6 @@ Base$Sub_metering_2 <- as.numeric(as.character(Base$Sub_metering_2))
 Base$Sub_metering_3 <- as.numeric(as.character(Base$Sub_metering_3))
 Base$Datetime <-paste(Base$Date, Base$Time)
 
-#Plot1.png
-
-png("plot1.png", width=480, height=480)
-hist(Base$Global_active_power, col="red", main="Global Active Power",xlab="Global Active Power (kilowatts)")
-dev.off()
-
-#Plot2.png
-
-png("plot2.png", width=480, height=480)
-plot(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Global_active_power, type="l", xlab="",ylab="Global Active Power(Kilowatts)" )
-dev.off()
-
-#Plot3.png
-
-
-
-png("plot3.png", width=480, height=480)
-plot(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
-lines(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Sub_metering_2, type = "l", col="red")
-lines(strptime(Base$Datetime, "%d/%m/%Y %H:%M:%S"), Base$Sub_metering_3, type = "l", col="blue")
-legend("topright",lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-dev.off()
 
 #Plot4.png
 
